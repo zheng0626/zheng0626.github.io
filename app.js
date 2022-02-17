820b2c8d6d04fc1ac330b6ab49b8c4dd3b72afcd
zheng0626 <junektz@hotmail.com>
2022-02-17 12:24:09 +0000

    experiment with Mysql promise

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const path = require('path');
+const cors= require('cors');
 const router = express.Router();
-const db = require('./config/db')
+// const db = require('./config/db')
+const dbService = require('./config/db')
 
 app.set('view engine', 'pug');
 app.set('views',path.join(__dirname,'views'));
 
 app.use(express.static(path.join(__dirname,'public')));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
 
 
 
@@ -19,9 +22,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 // })
 
 router.get('/',(req,res) =>{
-    category = []
-    res.render('index');
+    let fetchDb = new dbService();
+    console.log(fetchDb.getCatFoodData());
+    // categories.forEach(cat => {
+    //     let sql2 = `SELECT name,price FROM foods WHERE category_id = ${cat.id}`
+    //     categories_dict[cat.categoryName] = db.execute(sql2);
+    // });
+    // res.render('index',{
+    //     categoryNfood : categories_dict
+    // });
 });
+
+router.get('/getAll',(req,res)=>{
+    response.json({
+        success:true
+    });
+})
 
 router.get('/combinationPlatter',(req, res)=>{
     res.render('combinationPlatter');
--- config/db.js
+++ config/db.js
@@ -1,20 +1,81 @@
 require('dotenv').config();
+const { response } = require('express');
 const mysql = require('mysql2');
+let instance = null;
+
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user : process.env.DB_USER,
+    user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-});
+    password: process.env.DB_PASSWORD
+})
 
-let sql = "SELECT * FROM category;";
 
-console.log(process.env.DB_HOST);
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user : process.env.DB_USER,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.PORT
+// });
 
-pool.execute(sql, function(err,result){
-    if (err) throw err;
-    console.log(result);
-});
+// let sql = "SELECT * FROM category;";
 
-module.exports = pool.promise();
+// var categories_dict = {};
+// pool.execute(sql, function(err,result){
+//     if (err) throw err;
+//     // categories.forEach(cat => {
+
+//     //     categories_dict[cat.categoryName] = [];
+
+
+//     //     let sql2 = `SELECT name,price FROM foods WHERE category_id = ${cat.id}`
+//     //     pool.execute(sql2, (err2,foods) =>{
+//     //         if (err) throw err;
+//     //         categories_dict[cat.categoryName].push(foods);
+//     //     })
+//     // });
+//     return result;
+// });
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err.message);
+//     };
+//     console.log('db ' + connection.state);
+// })
+
+// pool.execute(sql,(err,result)=>{
+//     if(err) throw err;
+//     return result;
+// })
+
+class DbService{
+    static getDbServiceInstance(){
+        return instance ? instance : new DbService();
+    }
+
+    async getCatFoodData(){
+        const response = await new Promise((resolve, reject )=>{
+            const query = "SELECT * FROM category;";
+            pool.execute(query, (err,results)=>{
+                if (err) throw err;
+                resolve(results);
+            });
+        });
+
+        
+
+        console.log(response)
+        return response;
+    }
+}
+
+let database = new DbService();
+database.getCatFoodData();
+
+
+
+// module.exports = pool.promise();
+module.exports = DbService;
